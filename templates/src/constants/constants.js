44ec173331f44f71d9330ac876aa788e03e0cd65
Mr.Z <zrd0921@qq.com>
2020-12-19 10:03:22 +0800

    feat(templates): scroll alone

--- templates/src/constants/constants.js
+++ templates/src/constants/constants.js
@@ -13,3 +13,11 @@ export const REGION_KEY = 'region';
  * 全局地理位置参数的KEY
  */
 export const LOCATION_KEY = 'location';
+
+export const SCROLL_STATUS = {
+	0: '上拉加载', 
+	1: '加载中', 
+	2: '已全部加载', 
+	3: '网络不稳定，请稍后重试', 
+	4: '没有内容可供显示'
+};

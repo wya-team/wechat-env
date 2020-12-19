11161977edc538219c9827f15dae03966200b466
Mr.Z <zrd0921@qq.com>
2020-12-19 16:33:21 +0800

    feat(templates): component style

--- templates/src/common/component.js
+++ templates/src/common/component.js
@@ -1,20 +1,38 @@
 import storeMiddleware, { mapActions } from '@wya/mp-store';
 import { Utils } from '@wya/mp-utils';
 import net from '../utils/net';
+import { showMiddleware } from './common';
 
 let ComponentEnhancer = Utils.compose(
+	showMiddleware,
 	storeMiddleware,
 	// 其他注入
 )(Component);
 
-export default function (options = {}) {
-	
+export default function (opts = {}) {
+	const { externalClasses = [], options, properties, methods, ...restOpts } = opts;
+	if (!externalClasses.includes('custom-class')) {
+		externalClasses.push('custom-class');
+	}
 	return ComponentEnhancer({
-		...options,
+		...restOpts,
+		options: {
+			addGlobalClass: true,
+			multipleSlots: true,
+			...(options || {})
+		},
+		externalClasses,
+		properties: {
+			...(properties || {}),
+			customStyle: String
+		},
 		methods: {
-			...options.methods,
+			...(methods || {}),
 			$request: net.ajax,
 			...mapActions(['request']),
+			$emit(...args) {
+				this.triggerEvent(...args);
+			},
 		}
 	});
 }

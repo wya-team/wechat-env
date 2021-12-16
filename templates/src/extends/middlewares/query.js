939886e98c30cfd3ffc9d6632a657d5dc5c2abee
ScoutYin <scoutyin.1900@gmail.com>
2021-12-16 23:49:39 +0800

    fix(template): query middleware await

--- templates/src/extends/middlewares/query.js
+++ templates/src/extends/middlewares/query.js
@@ -1,18 +1,23 @@
 /**
- * 用于给页面注入$query
+ * 用于解析页面query，并给页面注入$query
  */
 import Mol from '@wya/mol';
 
 export default (next) => userOptions => {
 	const { onLoad, ...rest } = userOptions;
 
-	const proxy = hook => {
+	const patch = hook => {
 		return async function (query) {
-			const queryParser = Mol.queryParser;
-			this.$query = queryParser ? await queryParser.parse(query) : query;
-			hook && hook.call(this, this.$query);
+			hook && hook.call(this, query);
+			try {
+				// 中间件内不允许在生命周期hook前await，不然会将该hook多延后一个微任务队列
+				// 需要等待的逻辑都可添加到生命周期等待任务中
+				this.$query = await Mol.queryParser.parse(query);
+			} catch (error) {
+				this.$query = query;
+			}
 		};
 	};
-	rest.onLoad = proxy(onLoad);
+	rest.onLoad = patch(onLoad);
 	return next(rest);
 };

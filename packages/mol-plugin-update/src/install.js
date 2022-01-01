0b1af22f87683efdf044161d9e6ab02a3e7633f9
ScoutYin <scoutyin.1900@gmail.com>
2022-01-01 22:54:42 +0800

    fix: update tip text

--- packages/mol-plugin-update/src/install.js
+++ packages/mol-plugin-update/src/install.js
@@ -17,8 +17,8 @@ class UpdateManager {
 
 		this.manager.onUpdateReady(() => { 
 			wx.showModal({
-				title: '更新提示',
-				content: '新版本已准备就绪，是否重启应用？',
+				title: '版本更新',
+				content: '有新版本更新啦，是否立即重启应用？',
 				success: (res) => {
 					if (res.confirm) {
 						// 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
